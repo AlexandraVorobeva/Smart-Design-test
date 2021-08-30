d93a777c8cdd6aecb49dc8d76fa3ad5317350667
Alexandra Vorobeva <65659002+SparklingAcidity@users.noreply.github.com>
2021-08-30 15:41:50 +0300

    Delete src directory

--- src/models/product.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class Product(BaseModel):
-    id: int
-    name: str
-    description: str
-    options: dict
--- src/routs/__init__.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from .product import router as product_router
-
-router = APIRouter()
-router.include_router(product_router)
